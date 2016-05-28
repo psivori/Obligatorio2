9b85186547eea5ae14b8cea15aaf176675578948
Administrador <administrador@SP16.local>
2016-05-27 21:22:42 -0300

    casteo de fecha

--- SegundoObligatorio/Forecast.swift
+++ SegundoObligatorio/Forecast.swift
@@ -20,9 +20,10 @@ import ObjectMapper
 class Forecast: Mappable {
         
     var wheater: NSArray?
-    var day: NSNumber?
+    var date: NSDate!
     var temp: NSNumber?
     var icon: NSString?
+    var day: String?
     required init?(_ map: Map) {
         
         
@@ -31,9 +32,14 @@ class Forecast: Mappable {
     func mapping(map: Map) {
         self.wheater <-  map["weather"]
         self.icon = (self.wheater?.firstObject as! NSDictionary)["icon"] as! NSString
-        self.day <- map["dt"]
+        self.date <- (map["dt"], DateTransform())
         self.temp <- map["temp.day"]
-    
+        
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([.Weekday], fromDate: date)
+        
+        //let myComponents = myCalendar?.components(.WeekdayCalendarUnit, fromDate: self.date)
+        self.day = components?.Weekday
     }
     
     
